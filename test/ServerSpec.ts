e4b8b36deeab5f14c876f110a730d25f3c25929a
Eric Chan <ericchan_777@hotmail.com>
2017-02-27 06:55:17 -0800

    Added Server Tests

---
+++ test/ServerSpec.ts
@@ -0,0 +1,31 @@
+import { expect } from 'chai';
+import Log from "../src/Util";
+import Server from "../src/rest/Server";
+
+
+
+describe("ServerSpec", function () {
+    var s:Server = new Server(1234);
+
+    // files needed encoded in base 64
+    // files encoding ends here
+
+    it("Testing Stop Server", function () {
+        return s.stop().then(function (done) {
+            done;
+        }).catch(function (err) {
+            Log.test(err);
+        })
+    });
+
+    it("Testing Start Server", function () {
+        return s.start().then(function (done) {
+            done;
+        }).catch(function (err) {
+            Log.test(err);
+        })
+    });
+
+
+
+})
