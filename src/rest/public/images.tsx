cf462251e18b330092131748713e323502b17d51
craigyu <craig.yu93@gmail.com>
2017-03-22 04:21:45 -0700

    Tab now shows UBC icon

--- src/rest/public/images.tsx
+++ src/rest/public/images.tsx
@@ -5,7 +5,6 @@ import { title, course } from "./lib";
 export const title_image = (
      <div> <img src="http://i.imgur.com/8lNZ8o7.png" style={{display: "block", border:0, margin: 0, clear:"both" }} />{course}
          </div>
-         
 )
 
 
